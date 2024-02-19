6a38fc1d1bdaae5070a6b6bfb85c7cab00fb92bf
Vasu <vasukumar962@gmail.com>
2024-02-19 14:35:04 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022) 
+        return income * 1.2;
+        return income * 1.3;
+}
+calculateTax(10_000);
